8a36c3763675fe206496c50b6a898e6b16a5128e
root <root@e2e-113-47.ssdcloudindia.net>
2024-07-01 16:14:34 +0530

    made changes to work on production

--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://app.club-unplugged.com/dash/',
+  debug : true,
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  logger: {
+    level: 'debug',
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
